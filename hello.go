1f98081f55eeeaa7a406009b11779eeaacc65fd0
OKh <hudoshin7605@gmail.com>
2025-08-07 13:57:38 +0200

    bool

--- hello.go
+++ hello.go
@@ -10,6 +10,7 @@ func main() {
 		myFloat         float32 = 3.14
 		myInt           int     = 17
 		myInt32         int32   = 787
+		isActive        bool    = true
 	)
 	fmt.Println("Hello, World!")
 	fmt.Println("Hello, Golang!")
@@ -24,4 +25,5 @@ func main() {
 	fmt.Printf("value: %f, type %T \n", myFloat, myFloat)
 	fmt.Printf("value: %d, type %T \n", myInt, myInt)
 	fmt.Printf("Signet Integer (int): %d, type %T \n", myInt32, myInt32)
+	fmt.Printf("value: %v, type %T \n", isActive, isActive)
 }
