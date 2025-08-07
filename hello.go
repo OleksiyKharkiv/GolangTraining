03561bf2581ea3e3cc4376c10b6b117d53fe2f71
OKh <hudoshin7605@gmail.com>
2025-08-07 09:06:10 +0200

    Printf

--- hello.go
+++ hello.go
@@ -3,8 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	var (
+		companyName     string = "GoTraining"
+		yearEstablished int    = 2017
+		name                   = "Oleks"
+	)
 	fmt.Println("Hello, World!")
 	fmt.Println("Hello, Golang!")
 	a, b, c, d := 1, 2, 3, 4
 	fmt.Print(a, b, c, d)
+	fmt.Println()
+	fmt.Println(companyName, " ", yearEstablished)
+	fmt.Printf("Hello %s, \n welcome to %s \n in year %d !", name, companyName, yearEstablished)
 }
