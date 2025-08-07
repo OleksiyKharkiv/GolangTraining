786242058a226099151bbbbda7b11c52b0c73934
OKh <hudoshin7605@gmail.com>
2025-08-07 13:19:57 +0200

    Float, Int, and String Types

--- hello.go
+++ hello.go
@@ -4,9 +4,11 @@ import "fmt"
 
 func main() {
 	var (
-		companyName     string = "GoTraining"
-		yearEstablished int    = 2017
-		name                   = "Oleks"
+		companyName     string  = "GoTraining"
+		yearEstablished int     = 2017
+		name                    = "Oleks"
+		myFloat         float32 = 3.14
+		myInt           int     = 17
 	)
 	fmt.Println("Hello, World!")
 	fmt.Println("Hello, Golang!")
@@ -16,4 +18,7 @@ func main() {
 	fmt.Println(companyName, " ", yearEstablished)
 	fmt.Printf("Hello %s, \n welcome to %s \n in year %d ! \n", name, companyName, yearEstablished)
 	fmt.Printf("value: %s, type %T", companyName, companyName)
-}
+	fmt.Printf("value: %d, type %T", yearEstablished, yearEstablished)
+	fmt.Printf("value: %s, type %T", name, name)
+	fmt.Printf("value: %d, type %T", myFloat, myFloat)
+	fmt.Printf("value: %d, type %T", myInt, myInt)
